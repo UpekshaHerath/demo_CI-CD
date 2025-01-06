2d7a0bbfa61e01b2d000a81a7e88414387f095c4
UpekshaHerath <upekshadilshan000@gmail.com>
2025-01-06 16:39:09 +0530

    root route controller

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/")
+    public String index() {
+        return "Root route";
+    }
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World";
